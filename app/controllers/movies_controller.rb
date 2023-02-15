f3f97cb1d6cc017abd0c47b77143ca281c39d801
+ <+>
2023-02-15 13:14:59 -0500

    commit tree 66fee06a77972e494223a79dc173789c14cb4e8f

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,17 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create 
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
+
+  private
+  
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
 end
